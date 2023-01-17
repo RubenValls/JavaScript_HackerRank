59a48a6febc2f2d194c568aa0c476a6b0a3f7f5b
Rubén Valls <r.vallsaparici@gmail.com>
2023-01-17 09:29:57 +0100

    breakingTheRecords problem

---
+++ breakingTheRecords/breakingTheRecords.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+function breakingRecords(scores) {
+    // Write your code here
+    let minScore = scores[0];
+    let maxScore = scores[0];
+    let bestRecord = 0;
+    let worstRecord = 0;
+    
+    scores.forEach(score =>{
+        if(score < minScore){
+            worstRecord++;
+            minScore = score;
+        } else if(score > maxScore){
+            bestRecord++;
+            maxScore = score;
+        }
+    })
+    
+    let maxAndMin = [bestRecord, worstRecord];
+    return maxAndMin;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const scores = readLine().replace(/\s+$/g, '').split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
