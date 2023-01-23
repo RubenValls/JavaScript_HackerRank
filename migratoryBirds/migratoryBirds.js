990cb943a6641f5e3f7a687447b99d191b5c4267
Rubén Valls <r.vallsaparici@gmail.com>
2023-01-23 16:01:22 +0100

    migratoryBirds problem

---
+++ migratoryBirds/migratoryBirds.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function migratoryBirds(arr) {
+    // Write your code here
+    let maxCount = 0;
+    let birdType;
+    for (let birdClass = 1; birdClass < 6; birdClass++){
+        let count = arr.filter(x => x === birdClass).length
+        if(count > maxCount){
+            birdType = birdClass;
+            maxCount = count;
+        }
+    }
+    return birdType;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const result = migratoryBirds(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
